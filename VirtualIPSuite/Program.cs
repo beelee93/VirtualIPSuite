ba2c308df367941533b54951afbe9f64f096c4e6
Ong Bee Lee <OngBee.Lee@ramco.com>
2019-09-25 21:28:46 +0800

    Add project files.

---
+++ VirtualIPSuite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VirtualIPSuite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
